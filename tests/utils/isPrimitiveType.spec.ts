8b5992b4bb01a4c0f3ae85a552fc93ab63ee6cb8
Michal Kutateladze <michaeladze@returns.co>
2022-11-21 12:04:44 +0200

    Tests

---
+++ tests/utils/isPrimitiveType.spec.ts
@@ -0,0 +1,68 @@
+import { isPrimitiveType } from '../../src';
+
+describe('Test isPrimitiveType function', () => {
+
+  it('should return true for number', () => {
+    expect(isPrimitiveType('number')).toBeTruthy();
+  });
+
+  it('should return true for number[]', () => {
+    expect(isPrimitiveType('number[]')).toBeTruthy();
+  });
+
+  it('should return true for string', () => {
+    expect(isPrimitiveType('string')).toBeTruthy();
+  });
+
+  it('should return true for string[]', () => {
+    expect(isPrimitiveType('string[]')).toBeTruthy();
+  });
+
+  it('should return true for boolean', () => {
+    expect(isPrimitiveType('boolean')).toBeTruthy();
+  });
+
+  it('should return true for boolean[]', () => {
+    expect(isPrimitiveType('boolean[]')).toBeTruthy();
+  });
+
+  it('should return true for null', () => {
+    expect(isPrimitiveType('null')).toBeTruthy();
+  });
+
+  it('should return true for null[]', () => {
+    expect(isPrimitiveType('null[]')).toBeTruthy();
+  });
+
+  it('should return true for undefined', () => {
+    expect(isPrimitiveType('undefined')).toBeTruthy();
+  });
+
+  it('should return true for undefined[]', () => {
+    expect(isPrimitiveType('undefined[]')).toBeTruthy();
+  });
+
+  it('should return true for unknown', () => {
+    expect(isPrimitiveType('unknown')).toBeTruthy();
+  });
+
+  it('should return true for unknown[]', () => {
+    expect(isPrimitiveType('unknown[]')).toBeTruthy();
+  });
+
+  it('should return true for any', () => {
+    expect(isPrimitiveType('any')).toBeTruthy();
+  });
+
+  it('should return true for any[]', () => {
+    expect(isPrimitiveType('any[]')).toBeTruthy();
+  });
+
+  it('should return true for void', () => {
+    expect(isPrimitiveType('void')).toBeTruthy();
+  });
+
+  it('should return true for void[]', () => {
+    expect(isPrimitiveType('void[]')).toBeTruthy();
+  });
+});
