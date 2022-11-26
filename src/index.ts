540f2b078ddab998d2582a897ecefced63b97f9f
Michal Kutateladze <michaeladze@returns.co>
2022-11-26 19:44:54 +0200

    config types delaration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import {
+  IConfig, IConfigVariables
+} from './types/config.types';
+import {
   getTypeValue, isPrimitiveType, isArrayType
 } from './utils/basicTypes';
 import { capitalize } from './utils/capitalize';
@@ -9,3 +12,7 @@ export {
   isPrimitiveType,
   isArrayType
 };
+
+export interface CreatorConfig extends Omit<IConfig, 'variables'> {
+  variables?: IConfigVariables;
+}
