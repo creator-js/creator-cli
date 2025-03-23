0b74036a1c3664d4e3418bd04409a4dcee734980
Michal Kutateladze <michaeladze@returns.co>
2025-03-23 14:45:21 +0400

    fix: revert

--- src/utils/dynamicImport.ts
+++ src/utils/dynamicImport.ts
@@ -4,7 +4,7 @@ import { normalizePath } from './mk';
 export const dynamicImport = async (path: string) => {
   try {
     const winPrefix = process.platform === 'win32' ? 'file://' : '';
-    return eval(`import('${winPrefix}${normalizePath(path)}');`);
+    return await import(`${winPrefix}${normalizePath(path)}`);
   } catch (e) {
     logger.info(e);
     logger.error('Error in dynamicImport() function');
